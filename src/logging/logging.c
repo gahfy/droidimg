a96789dee62c092a5a89c637e4bae599b5bc897b
Gahfy <g.herfray@gahfy.io>
2022-11-27 14:21:28 +0700

    Fix gcc warning about log function

--- src/logging/logging.c
+++ src/logging/logging.c
@@ -5,40 +5,40 @@
 /** The current level of logs in use in the application */
 static log_level current_level = Warning;
 
-static int log(log_level level, char *format, va_list arguments);
+static int print_log(log_level level, char *format, va_list arguments);
 
 int logv(char *format, ...) {
     va_list args;              // Use argument a somehow
     va_start(args, format);
-    return log(Verbose, format, args);
+    return print_log(Verbose, format, args);
     va_end(args);
 }
 
 int logd(char *format, ...) {
     va_list args;              // Use argument a somehow
     va_start(args, format);
-    return log(Debug, format, args);
+    return print_log(Debug, format, args);
     va_end(args);
 }
 
 int logi(char *format, ...) {
     va_list args;              // Use argument a somehow
     va_start(args, format);
-    return log(Info, format, args);
+    return print_log(Info, format, args);
     va_end(args);
 }
 
 int logw(char *format, ...) {
     va_list args;              // Use argument a somehow
     va_start(args, format);
-    return log(Warning, format, args);
+    return print_log(Warning, format, args);
     va_end(args);
 }
 
 int loge(char *format, ...) {
     va_list args;              // Use argument a somehow
     va_start(args, format);
-    return log(Error, format, args);
+    return print_log(Error, format, args);
     va_end(args);
 }
 
@@ -57,7 +57,7 @@ void set_log_level(log_level new_level) {
  *
  * @return the number of characters that have been printed
  */
-int log(log_level level, char *format, va_list arguments) {
+int print_log(log_level level, char *format, va_list arguments) {
     if(level >= current_level) {
         return vfprintf(level >= Error ? stderr : stdout, format, arguments);
     }
